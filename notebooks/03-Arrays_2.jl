fbd13e223b33873cb618b2c46280454f90a229d0
Abhro <5664668+abhro@users.noreply.github.com>
2025-11-04 09:47:42 -0500

    Create stub for notebooks/03-Arrays_2.jl

---
+++ notebooks/03-Arrays_2.jl
@@ -0,0 +1,51 @@
+### A Pluto.jl notebook ###
+# v0.20.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 606d3a60-b987-11f0-b09c-ab2e3fa2835d
+using DrWatson
+
+# ╔═╡ 3e21eeef-836b-468e-b058-9c26d8f75f01
+@quickactivate "AAS247Julia"
+
+# ╔═╡ 157fdc89-ff6f-4e6c-99b0-fc952daa24ef
+using PlutoUI
+
+# ╔═╡ c47564b1-751b-46e0-883f-86fc11f6abb8
+notebookName = "Arrays 2";
+
+# ╔═╡ 56274a04-6ace-477a-a4f4-3acad1a85809
+TableOfContents(title = notebookName)
+
+# ╔═╡ 2788d2f9-a6b9-4169-85c9-014e5dd565bc
+md"""
+## Array and tuple comprehensions
+"""
+
+# ╔═╡ 7a634fe5-2a4e-4abd-90c8-5a4da31bba1e
+md"""
+## Array comprehensions with "if statement"
+"""
+
+# ╔═╡ bfa7975e-f206-4179-93eb-e0236855c981
+md"""
+## Array slicing & indexing
+"""
+
+# ╔═╡ d1e8e027-ce75-4cee-bbbd-f977e7c4d442
+md"""
+## Array slicing with boolean array (masking)
+"""
+
+# ╔═╡ Cell order:
+# ╟─606d3a60-b987-11f0-b09c-ab2e3fa2835d
+# ╟─3e21eeef-836b-468e-b058-9c26d8f75f01
+# ╟─157fdc89-ff6f-4e6c-99b0-fc952daa24ef
+# ╟─c47564b1-751b-46e0-883f-86fc11f6abb8
+# ╟─56274a04-6ace-477a-a4f4-3acad1a85809
+# ╟─2788d2f9-a6b9-4169-85c9-014e5dd565bc
+# ╟─7a634fe5-2a4e-4abd-90c8-5a4da31bba1e
+# ╟─bfa7975e-f206-4179-93eb-e0236855c981
+# ╟─d1e8e027-ce75-4cee-bbbd-f977e7c4d442
